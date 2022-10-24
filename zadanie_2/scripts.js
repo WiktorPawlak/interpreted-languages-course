a2cf4715dde9f0e8f3b270848a6856f52369cfc0
WiktorPawlak <pawlak4112@wp.pl>
2022-10-25 00:21:52 +0200

    add jQuery

--- zadanie_2/scripts.js
+++ zadanie_2/scripts.js
@@ -1,42 +1,42 @@
 "use strict"
-let todoList = []; 
+let todoList = [];
 
 //initialize to do list from choosen JSON bin
-let initList = function() {
+let initList = function () {
     $.ajax({
         url: 'https://api.jsonbin.io/v3/b/63480b432b3499323bdd3b06',
         type: 'GET',
-        headers: { 
-          'X-Master-Key': '$2b$10$0t5LHPGAMAjkhpD1iqTWauGoxYtp.Rlbe/UzLJqi5R6CvKlfY3e5e'
+        headers: {
+            'X-Master-Key': '$2b$10$0t5LHPGAMAjkhpD1iqTWauGoxYtp.Rlbe/UzLJqi5R6CvKlfY3e5e'
         },
         success: (data) => {
-         todoList = data.record;
-         updateTodoList();
-         console.log(data.record);
-        }, 
+            todoList = data.record;
+            updateTodoList();
+            console.log(data.record);
+        },
         error: (err) => {
-          console.log(err.responseJSON);
+            console.log(err.responseJSON);
         }
-       });
+    });
 }
 
 initList();
 
-let updateJSONbin = function() {
+let updateJSONbin = function () {
     $.ajax({
-    url: 'https://api.jsonbin.io/v3/b/63480b432b3499323bdd3b06',
-    type: 'PUT',
-    headers: { 
-        'X-Master-Key': '$2b$10$0t5LHPGAMAjkhpD1iqTWauGoxYtp.Rlbe/UzLJqi5R6CvKlfY3e5e'
-    },
-    contentType: 'application/json',
-    data: JSON.stringify(todoList),
-    success: (data) => {
-        console.log(data);
-    },
-    error: (err) => {
-        console.log(err.responseJSON);
-    }
+        url: 'https://api.jsonbin.io/v3/b/63480b432b3499323bdd3b06',
+        type: 'PUT',
+        headers: {
+            'X-Master-Key': '$2b$10$0t5LHPGAMAjkhpD1iqTWauGoxYtp.Rlbe/UzLJqi5R6CvKlfY3e5e'
+        },
+        contentType: 'application/json',
+        data: JSON.stringify(todoList),
+        success: (data) => {
+            console.log(data);
+        },
+        error: (err) => {
+            console.log(err.responseJSON);
+        }
     });
 }
 
@@ -48,7 +48,7 @@ let updateTodoList = function () {
 
     //remove all elements
     table.empty();
- 
+
     for (let todo in todoList) {
         if (SearchFilter(filterInput, todoList[todo])) {
             searchItemList.push(todoList[todo]);
@@ -69,61 +69,58 @@ let updateTodoList = function () {
     }
 }
 
-let SearchFilter = function(filterInput, todoObject) {
-    let filterInputValue = document.getElementById("inputSearch");
-    if ((filterInputValue.value == "") ||
-    todoObject.title.includes(filterInput) ||
-    todoObject.description.includes(filterInput) ||
-    todoObject.place.includes(filterInput)) {
-        return true;
-    }
-    return false;
+let SearchFilter = function (filterInput, todoObject) {
+    let filterInputValue = $("input.inputSearch").val();
+    return !!((filterInputValue == "") ||
+        todoObject.title.includes(filterInput) ||
+        todoObject.description.includes(filterInput) ||
+        todoObject.place.includes(filterInput));
 };
 
 
 //sarch tasks based on the search input 
-let Search = function() {
+let Search = function () {
     updateTodoList();
 }
 
 //clear tasks shown after using Search() function
 let Clear = function () {
-    let filterInput = document.getElementById("inputSearch");   
+    let filterInput = $("document.inputSearch");
     filterInput.value = "";
     updateTodoList();
 }
 
 //delete task
-let deleteTodo = function(index) {
-    todoList.splice(index,1);
+let deleteTodo = function (index) {
+    todoList.splice(index, 1);
     //update tasks on viewport and JSON bin
     updateJSONbin();
     updateTodoList();
 }
 
-let addTodo = function() {
+let addTodo = function () {
     //get the elements in the form
-      let inputTitle = document.getElementById("inputTitle");
-      let inputDescription = document.getElementById("inputDescription");
-      let inputPlace = document.getElementById("inputPlace");
-      let inputDate = document.getElementById("inputDate");
+    let inputTitle = $("document.inputTitle");
+    let inputDescription = $("document.inputDescription");
+    let inputPlace = $("document.inputPlace");
+    let inputDate = $("document.inputDate");
     //get the values from the form
-      let newTitle = inputTitle.value;
-      let newDescription = inputDescription.value;
-      let newPlace = inputPlace.value;
-      let newDate = new Date(inputDate.value);
-      newDate = newDate.getUTCDate() + "." + (newDate.getMonth()+1) + "." + (newDate.getFullYear());
+    let newTitle = inputTitle.value;
+    let newDescription = inputDescription.value;
+    let newPlace = inputPlace.value;
+    let newDate = new Date(inputDate.value);
+    newDate = newDate.getUTCDate() + "." + (newDate.getMonth() + 1) + "." + (newDate.getFullYear());
     //create new item
-      let newTodo = {
-          title: newTitle,
-          description: newDescription,
-          place: newPlace,
-          dueDate: newDate
-      };
-      //add item to the list
-      todoList.push(newTodo);
-      window.localStorage.setItem("todos", JSON.stringify(todoList));
-      //update tasks on viewport and JSON bin
-      updateJSONbin();
-      updateTodoList();
-  }
+    let newTodo = {
+        title: newTitle,
+        description: newDescription,
+        place: newPlace,
+        dueDate: newDate
+    };
+    //add item to the list
+    todoList.push(newTodo);
+    window.localStorage.setItem("todos", JSON.stringify(todoList));
+    //update tasks on viewport and JSON bin
+    updateJSONbin();
+    updateTodoList();
+}
